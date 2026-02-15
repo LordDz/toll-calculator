dec63e5eb018ecc7cf2c215dad464f8a2f053261
David Zetterdahl <david_zetterdahl@hotmail.com>
2026-02-15 22:59:24 +0100

    Less jumpy text

--- client/src/pages/toll/components/AddPassageSection/FeeCost/FeeCostCard/FeeCostContent/index.tsx
+++ client/src/pages/toll/components/AddPassageSection/FeeCost/FeeCostCard/FeeCostContent/index.tsx
@@ -1,14 +1,22 @@
 import type { FeeCheckResult } from '@/api/queries/toll'
 import { useEffect, useState } from 'react'
-import type { FeeCostContentProps } from './index.types'
 import { FeeCostDisplay } from './FeeCostDisplay'
+import type { FeeCostContentProps } from './index.types'
+
+const skipUpdate = (data: FeeCheckResult | undefined, displayData: FeeCheckResult | undefined) => {
+  if (!data) return true;
+  if (!displayData) return false;
+  return data.feeSek === displayData.feeSek && data.isFree === displayData.isFree && data.reason === displayData.reason;
+}
 
 export const FeeCostContent = ({ data }: FeeCostContentProps) => {
   const [displayData, setDisplayData] = useState<FeeCheckResult | undefined>(data)
   const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
-    if (data === displayData) return
+
+    if (skipUpdate(data, displayData)) return;
+
     setIsVisible(false)
     const startTransition = setTimeout(() => {
       setDisplayData(data)
